e369d04d91dddfd5dcbb8f221a6c16ec3b998360
cosmah <cosmahke4@gmail.com>
2024-09-20 00:10:28 +0300

    hosts

--- criminal_system/criminal_system/settings.py
+++ criminal_system/criminal_system/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-3d+gh((o=t7q*8j05=gmp%)k2j6v3i*y09@=tvzlv7n@(*%2%i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
