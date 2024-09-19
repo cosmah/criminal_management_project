2c68b35130cd4f642ac4c6162603370dd0304d5f
cosmah <cosmahke4@gmail.com>
2024-09-19 11:59:21 -0700

    haved added styling to the templates, all remaining is substituting in the google map api

--- criminal_system/app/urls.py
+++ criminal_system/app/urls.py
@@ -1,11 +1,17 @@
 # app/urls.py
 
 from django.urls import path
-from .views import criminal_record_list, criminal_record_detail, criminal_record_search, citizen_match
+from .views import criminal_record_detail, criminal_record_search, citizen_match, criminal_record_list
 
 urlpatterns = [
-    path('', criminal_record_list, name='criminal_record_list'),
+    # Match URL for citizen_match view
+    path('', citizen_match, name='citizen_match'),
+    
+    # Match URL for criminal record detail view
     path('record/<int:pk>/', criminal_record_detail, name='criminal_record_detail'),
+    
+    # Match URL for search functionality
     path('search/', criminal_record_search, name='criminal_record_search'),
-    path('match/', citizen_match, name='citizen_match'),  # Ensure this line is present
+
+    path('criminal-records/', criminal_record_list, name='criminal_record_list'),
 ]
